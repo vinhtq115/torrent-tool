55cb7210c5995d35335b9acb0e51e3801e1477c2
Vinh Quang Tran <starcraft6723@hotmail.com>
2021-09-19 12:53:37 +0700

    Fixed and improved Encoder and Decoder.

--- Bencode/Decoder.py
+++ Bencode/Decoder.py
@@ -14,49 +14,58 @@ class BencodeDecoder:
     def __init__(self, encoding='UTF-8'):
         self.encoding = encoding
         self.ops = {
-            list: self.__decode_string,
-            int: self.__decode_int,
-            dict: self.__decode_dictionary,
-            str: self.__decode_string
+            list: self.decode_list,
+            int: self.decode_int,
+            dict: self.decode_dictionary,
+            bytes: self.decode_string_bytes
         }
+        self.__char_i = 'i'.encode(self.encoding)[0]
+        self.__char_l = 'l'.encode(self.encoding)[0]
+        self.__char_d = 'd'.encode(self.encoding)[0]
+        self.__char_e = 'e'.encode(self.encoding)[0]
+        self.__char_0 = '0'.encode(self.encoding)[0]
+        self.__char_9 = '9'.encode(self.encoding)[0]
+        self.__char_colon = ':'.encode(self.encoding)[0]
+        self.__char_hyphen = '-'.encode(self.encoding)[0]
 
-    def __decode_int(self, inp: str):
+    def decode_int(self, inp: bytes):
         """
         Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'.
         For example i3e corresponds to 3 and i-3e corresponds to -3. Integers have no size limitation. i-0e is invalid.
         All encodings with a leading zero, such as i03e, are invalid, other than i0e, which of course corresponds to 0.
-        :param inp: Input string
+        :param inp: Input bytes string
         :return: Decoded integer
         """
-        if inp[0] != 'i' or inp[-1] != 'e':
+        if inp[0] != self.__char_i or inp[-1] != self.__char_e:
             raise DecodeError(f'To decode integer, input must begin with \'i\' and end with \'e\'. '
-                              f'Input: \'{inp}\'')
+                              f'Input: {inp}.')
 
         try:
             result = int(inp[1: -1])
         except ValueError:
             raise DecodeError(f'Integer must be encoded in base ten ASCII. '
-                              f'Input: \'{inp}\'')
+                              f'Input: {inp}.')
 
-        if result == 0 and inp[1] == '-':
-            raise DecodeError(f'Negative zero is not permitted. Input: \'{inp}\'')
+        if result == 0 and inp[1] == self.__char_hyphen:
+            raise DecodeError(f'Negative zero is not permitted. Input: {inp}.')
 
-        if result != 0 and ((inp[1] == '-' and inp[2] == '0') or (inp[1] == '0')):
-            raise DecodeError(f'Leading zeros are not allowed. Input: \'{inp}\'')
+        if result != 0 and ((inp[1] == self.__char_hyphen and inp[2] == self.__char_0)
+                            or (inp[1] == self.__char_0)):
+            raise DecodeError(f'Leading zeros are not allowed. Input: {inp}.')
 
         return result
 
-    def __decode_string(self, inp: str):
+    def decode_string_bytes(self, inp: bytes):
         """
         Strings are length-prefixed base ten followed by a colon and the string.
         For example 4:spam corresponds to 'spam'.
-        :param inp: Input string
-        :return: Decoded string
+        :param inp: Input bytes string
+        :return: Decoded bytes string
         """
-        colon_pos = inp.find(':')
+        colon_pos = inp.find(self.__char_colon)
 
         if colon_pos == -1:
-            raise DecodeError(f'String must contain colon \':\'. Input: \'{inp}\'')
+            raise DecodeError(f'String must contain colon \':\'. Input: {inp}.')
 
         try:
             string_length = int(inp[:colon_pos])
@@ -66,163 +75,182 @@ class BencodeDecoder:
             raise DecodeError(f'Prefixed string before colon must be an integer and higher than 0. Input: \'{inp}\'')
 
         if string_length != len(inp[colon_pos + 1:]):
-            raise DecodeError(f'String length and predefined length do not match. Input: \'{inp}\'')
+            raise DecodeError(f'String length and predefined length do not match. '
+                              f'String length: {string_length}, predefined length: {len(inp[colon_pos + 1:])}. '
+                              f'Input: \'{inp}\'')
 
-        return str(inp[colon_pos + 1:])
+        return inp[colon_pos + 1:]
 
-    def __decode_list(self, inp: str):
+    def decode_list(self, inp: bytes):
         """
         Lists are encoded as an 'l' followed by their elements (also bencoded) followed by an 'e'.
         For example l4:spam4:eggse corresponds to ['spam', 'eggs'].
-        :param inp: Input string
+        :param inp: Input bytes string
         :return: Decoded list
         """
-        if inp[0] != 'l' or inp[-1] != 'e':
+        if inp[0] != self.__char_l or inp[-1] != self.__char_e:
             raise DecodeError(f'To decode list, input must begin with \'l\' and end with \'e\'. '
-                              f'Input: \'{inp}\'')
+                              f'Input: {inp}.')
 
         results = list()
         idx = 1
 
         while idx < len(inp) - 1:
-            if inp[idx] == 'i':  # Decode integer
-                ending_e = inp.find('e', idx + 1)
+            if inp[idx] == self.__char_i:  # Decode integer
+                ending_e = inp.find(self.__char_e, idx + 1)
                 inp_ = inp[idx: ending_e + 1]
-                idx = ending_e + 1
                 try:
-                    result = self.__decode_int(inp_)
+                    result = self.decode_int(inp_)
+                    idx = ending_e + 1
                 except DecodeError:
-                    raise DecodeError(f'Cannot decode integer from \'{inp_}\'.')
-            elif inp[idx] == 'l':  # Decode list
-                ending_e = inp.find('e', idx + 1)
-                inp_ = inp[idx: ending_e + 1]
-                idx = ending_e + 1
-                try:
-                    result = self.__decode_list(inp_)
-                except DecodeError:
-                    raise DecodeError(f'Cannot decode list from \'{inp_}\'.')
-            elif inp[idx] == 'd':  # Decode dictionary
-                ending_e = inp.find('e', idx + 1)
-                inp_ = inp[idx: ending_e + 1]
-                idx = ending_e + 1
-                try:
-                    result = self.__decode_list(inp_)
-                except DecodeError:
-                    raise DecodeError(f'Cannot decode dictionary from \'{inp_}\'.')
-            elif inp[idx].isnumeric():  # Decode string
-                colon = inp.find(':', idx + 1)
+                    raise DecodeError(f'Cannot decode integer from {inp_}.')
+            elif inp[idx] == self.__char_l:  # Decode list
+                ending_e = inp.find(self.__char_e, idx + 1)
+                if ending_e == -1:
+                    raise DecodeError(f'Cannot decode list from index {idx}.')
+                while ending_e != -1:
+                    inp_ = inp[idx: ending_e + 1]
+                    try:
+                        result = self.decode_list(inp_)
+                        idx = ending_e + 1
+                        break
+                    except DecodeError:
+                        ending_e = inp.find(self.__char_e, ending_e + 1)
+                    if ending_e == -1:
+                        raise DecodeError(f'Cannot decode list from {inp_}.')
+
+            elif inp[idx] == self.__char_d:  # Decode dictionary
+                ending_e = inp.find(self.__char_e, idx + 1)
+                if ending_e == -1:
+                    raise DecodeError(f'Cannot decode dictionary from index {idx}.')
+                while ending_e != -1:
+                    inp_ = inp[idx: ending_e + 1]
+                    try:
+                        result = self.decode_dictionary(inp_)
+                        idx = ending_e + 1
+                        break
+                    except DecodeError:
+                        ending_e = inp.find(self.__char_e, ending_e + 1)
+                    if ending_e == -1:
+                        raise DecodeError(f'Cannot decode dictionary from index {idx}.')
+            elif self.__char_0 <= inp[idx] <= self.__char_9:  # Decode string
+                colon = inp.find(self.__char_colon, idx + 1)
                 try:
                     str_len = int(inp[idx: colon])
                     if str_len <= 0:
                         raise ValueError
                 except ValueError:
-                    raise DecodeError(
-                        f'Prefixed string before colon must be an integer and higher than 0. Input: \'{inp}\'')
+                    raise DecodeError(f'Prefixed string before colon must be an integer and higher than 0. '
+                                      f'Input: {inp}.')
 
                 inp_ = inp[idx: colon + str_len + 1]
-                idx = colon + str_len + 1
                 try:
-                    result = self.__decode_string(inp_)
+                    result = self.decode_string_bytes(inp_)
+                    idx = colon + str_len + 1
                 except DecodeError:
-                    raise DecodeError(f'Cannot decode string from \'{inp_}\'.')
+                    raise DecodeError(f'Cannot decode string from {inp_}.')
             else:
-                raise DecodeError(f'Cannot infer type from character \'{inp[idx]}\' '
+                raise DecodeError(f'Cannot infer type from character {inp[idx]} '
                                   f'at index {idx} in input {inp}.')
             results.append(result)
 
         return results
 
-    def __decode_dictionary(self, inp: str):
+    def decode_dictionary(self, inp: bytes):
         """
         Dictionaries are encoded as a 'd' followed by a list of alternating keys and their corresponding values
         followed by an 'e'. For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} and
         d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. Keys must be strings and appear in sorted order
         (sorted as raw strings, not alphanumerics).
-        :param inp: Input string
+        :param inp: Input bytes string
         :return: Decoded dictionary
         """
-        if inp[0] != 'd' or inp[-1] != 'e':
+        if inp[0] != self.__char_d or inp[-1] != self.__char_e:
             raise DecodeError(f'To decode dictionary, input must begin with \'d\' and end with \'e\'. '
-                              f'Input: \'{inp}\'')
+                              f'Input: {inp}.')
 
         results = OrderedDict()
         keys = []
         idx = 1
 
-        while idx < len(inp) - 1:
+        while idx < len(inp) - 2:
             # Extract key (string)
-            if not inp[idx].isnumeric():
-                raise f'Dictionary key must conform to string specification. Found {inp[idx]} at index {idx}. ' \
-                      f'Input: \'{inp}\'.'
+            if not self.__char_0 <= inp[idx] <= self.__char_9:
+                raise DecodeError(f'Dictionary key must conform to string specification. '
+                                  f'Found {inp[idx]} at index {idx}. Input: {inp}.')
 
-            colon = inp.find(':', idx + 1)
+            colon = inp.find(self.__char_colon, idx + 1)
             try:
                 str_len = int(inp[idx: colon])
                 if str_len <= 0:
                     raise ValueError
             except ValueError:
                 raise DecodeError(
-                    f'Prefixed string before colon must be an integer and higher than 0. Input: \'{inp}\'')
+                    f'Prefixed string before colon must be an integer and higher than 0. Input: {inp}.')
             inp_ = inp[idx: colon + str_len + 1]
             idx = colon + str_len + 1
             try:
-                key = self.__decode_string(inp_)
+                key = self.decode_string_bytes(inp_)
             except DecodeError:
-                raise DecodeError(f'Cannot decode string from \'{inp_}\'.')
+                raise DecodeError(f'Cannot decode string from {inp_}.')
 
             # Extract value
-            if inp[idx] == 'i':  # Decode integer
-                ending_e = inp.find('e', idx + 1)
+            if inp[idx] == self.__char_i:  # Decode integer
+                ending_e = inp.find(self.__char_e, idx + 1)
                 inp_ = inp[idx: ending_e + 1]
                 idx = ending_e + 1
                 try:
-                    value = self.__decode_int(inp_)
+                    value = self.decode_int(inp_)
                 except DecodeError:
-                    raise DecodeError(f'Cannot decode integer from \'{inp_}\'.')
-            elif inp[idx] == 'l':  # Decode list
-                ending_e = inp.find('e', idx + 1)
+                    raise DecodeError(f'Cannot decode integer from {inp_}.')
+            elif inp[idx] == self.__char_l:  # Decode list
+                ending_e = inp.find(self.__char_e, idx + 1)
                 inp_ = inp[idx: ending_e + 1]
                 idx = ending_e + 1
                 try:
-                    value = self.__decode_list(inp_)
+                    value = self.decode_list(inp_)
                 except DecodeError:
-                    raise DecodeError(f'Cannot decode list from \'{inp_}\'.')
-            elif inp[idx] == 'd':  # Decode dictionary
-                ending_e = inp.find('e', idx + 1)
-                inp_ = inp[idx: ending_e + 1]
-                idx = ending_e + 1
-                try:
-                    value = self.__decode_list(inp_)
-                except DecodeError:
-                    raise DecodeError(f'Cannot decode dictionary from \'{inp_}\'.')
-            elif inp[idx].isnumeric():  # Decode string
-                colon = inp.find(':', idx + 1)
+                    raise DecodeError(f'Cannot decode list from {inp_}.')
+            elif inp[idx] == self.__char_d:  # Decode dictionary
+                ending_e = inp.find(self.__char_e, idx + 1)
+                if ending_e == -1:
+                    raise DecodeError(f'Cannot decode dictionary from index {idx}.')
+                while ending_e != -1:
+                    inp_ = inp[idx: ending_e + 1]
+                    try:
+                        value = self.decode_dictionary(inp_)
+                        idx = ending_e + 1
+                        break
+                    except DecodeError:
+                        # raise DecodeError(f'Cannot decode dictionary from {inp_}.')
+                        ending_e = inp.find(self.__char_e, ending_e + 1)
+                    if ending_e == -1:
+                        raise DecodeError(f'Cannot decode dictionary from index {idx}.')
+            elif self.__char_0 <= inp[idx] <= self.__char_9:  # Decode string
+                colon = inp.find(self.__char_colon, idx + 1)
                 try:
                     str_len = int(inp[idx: colon])
                     if str_len <= 0:
                         raise ValueError
                 except ValueError:
                     raise DecodeError(
-                        f'Prefixed string before colon must be an integer and higher than 0. Input: \'{inp}\'')
+                        f'Prefixed string before colon must be an integer and higher than 0. Input: {inp}.')
 
                 inp_ = inp[idx: colon + str_len + 1]
                 idx = colon + str_len + 1
                 try:
-                    value = self.__decode_string(inp_)
+                    value = self.decode_string_bytes(inp_)
                 except DecodeError:
-                    raise DecodeError(f'Cannot decode string from \'{inp_}\'.')
+                    raise DecodeError(f'Cannot decode string from {inp_}.')
             else:
                 raise DecodeError(f'Cannot infer type from character \'{inp[idx]}\' '
                                   f'at index {idx} in input {inp}.')
 
             results[key] = value
-            keys.append(key.encode(self.encoding))
+            keys.append(key)
 
         if keys != sorted(keys):
             raise DecodeError(f'Keys must be strings and appear in sorted order '
                               f'(sorted as raw strings, not alphanumerics).')
 
         return results
-
-    def decode(self, obj, op):
-        assert op in [list, int, dict, str]
--- Bencode/Encoder.py
+++ Bencode/Encoder.py
@@ -2,36 +2,58 @@
 from collections import OrderedDict
 
 
+class EncodeError(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
 class BencodeEncoder:
     def __init__(self, encoding='UTF-8'):
         self.encoding = encoding
         self.ops = {
-            int: self.__encode_int,
-            str: self.__encode_string,
-            dict: self.__encode_dict,
-            list: self.__encode_list
+            int: self.encode_int,
+            str: self.encode_string,
+            bytes: self.encode_string,
+            dict: self.encode_dict,
+            list: self.encode_list
         }
 
-    def __encode_int(self, obj: int):
+    def encode_int(self, obj: int) -> bytes:
         """
         Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'.
         For example i3e corresponds to 3 and i-3e corresponds to -3. Integers have no size limitation. i-0e is invalid.
         All encodings with a leading zero, such as i03e, are invalid, other than i0e, which of course corresponds to 0.
         :param obj: Integer number
-        :return: String of encoded number
+        :return: Bytes string of encoded number
         """
-        return f'i{obj}e'
+        return f'i{obj}e'.encode(self.encoding)
 
-    def __encode_dict(self, obj: dict):
+    def encode_string(self, obj: str or bytes) -> bytes:
+        """
+        Strings are length-prefixed base ten followed by a colon and the string.
+        For example 4:spam corresponds to 'spam'.
+        :param obj: String or byte string
+        :return: Bytes string of encoded string
+        """
+        if type(obj) is str:
+            obj = obj.encode(self.encoding)
+
+        return f'{len(obj)}:'.encode(self.encoding) + obj
+
+    def encode_dict(self, obj: dict) -> bytes:
         """
         Dictionaries are encoded as a 'd' followed by a list of alternating keys and their corresponding values
         followed by an 'e'. For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} and
         d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. Keys must be strings and appear in sorted order
         (sorted as raw strings, not alphanumerics).
-        :param obj:
-        :return:
+        :param obj: Dictionary
+        :return: Bytes string of encoded dictionary
         """
-        res = 'd'
+        res = 'd'.encode(self.encoding)
 
         # Convert dictionary's keys to bytes for sorting.
         byte_key_dict = dict()
@@ -42,43 +64,33 @@ class BencodeEncoder:
 
         # Encode
         for key, value in sorted_byte_key_dict.items():
-            res += self.__encode_string(key.decode(self.encoding, 'strict')) + self.ops[type(value)](value)
+            res += self.encode_string(key) + self.ops[type(value)](value)
 
-        res += 'e'
+        res += 'e'.encode(self.encoding)
         return res
 
-    def __encode_string(self, obj: str):
-        """
-        Strings are length-prefixed base ten followed by a colon and the string.
-        For example 4:spam corresponds to 'spam'.
-        :param obj: String
-        :return: String of encoded string
-        """
-        return f'{len(obj)}:{obj}'
-
-    def __encode_list(self, obj: list):
+    def encode_list(self, obj: list) -> bytes:
         """
         Lists are encoded as an 'l' followed by their elements (also bencoded) followed by an 'e'.
         For example l4:spam4:eggse corresponds to ['spam', 'eggs'].
         :param obj: List of elements
         :return: String of encoded list
         """
-        res = 'l'
+        res = 'l'.encode(self.encoding)
         for element in obj:
-            assert type(element) in [list, int, dict, str]
+            print(element)
+            if type(element) not in [list, int, dict, str, bytes]:
+                raise EncodeError(f'Unsupported element type \'{type(element)}\' of element \'{element}\'.')
+
             op = self.ops[type(element)]
             res += op(element)
-        res += 'e'
+        res += 'e'.encode(self.encoding)
         return res
 
-    def encode(self, obj):
-        assert type(obj) in [list, int, dict, str]
+    def encode(self, obj) -> bytes:
+        if type(obj) not in [list, int, dict, str, bytes]:
+            raise EncodeError(f'Unsupported object type \'{type(obj)}\' of object \'{obj}\'.')
+
         op = self.ops[type(obj)]
-        res = op(obj).encode(self.encoding)
+        res = op(obj)
         return res
-
-
-if __name__ == '__main__':
-    encoder = BencodeEncoder()
-    print(encoder.encode({'cow': 'moo', 'spam': 'eggs'}))
-    print(encoder.encode({'spam': ['a', 'b']}))
